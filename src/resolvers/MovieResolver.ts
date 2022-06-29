704bde190d0e2348e3e221af0ecc44752a02fc29
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-06-29 12:45:43 +0300

    CRUD Operation

---
+++ src/resolvers/MovieResolver.ts
@@ -0,0 +1,55 @@
+import { Movie } from "../entity/Movie";
+import { Arg, Field, InputType, Int, Mutation, Query, Resolver } from "type-graphql";
+
+
+@InputType()
+class MovieInput {
+    @Field()
+    title: string;
+
+    @Field(() => Int)
+    minutes: number
+}
+
+
+@InputType()
+class MovieUpdateInput {
+    @Field(() =>String, { nullable: true })
+    title?: string;
+
+    @Field(() => Int, {nullable: true })
+    minutes?: number;
+}
+
+
+
+@Resolver()
+ export class MovieResolver {
+    @Mutation(() => Movie)
+    async createMovie(@Arg("options", () => MovieInput) options: MovieInput,) {
+    const movie = await Movie.create(options).save();
+        return movie;
+    }
+
+    @Mutation(() => Boolean)
+    async updateMovie(
+        @Arg('id') id:number,
+        @Arg("input", () => MovieUpdateInput) input: MovieUpdateInput
+    ) {
+        await Movie.update({ id }, input);
+        return true
+    }
+
+    @Mutation(() => Boolean)
+    async deleteMovie(
+        @Arg("id", () => Int) id: number
+    ){
+        await Movie.delete({ id });
+        return true;
+    }
+
+    @Query(() => [Movie])
+    movies() {
+        return Movie.find();
+    }
+}
